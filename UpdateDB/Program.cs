54c8de54f457a5fa9474ca8289d13e07cc738f09
andrej-koman <andrej.koman123@gmail.com>
2024-11-02 01:58:54 +0100

    feat: Added UpdateDb project

---
+++ UpdateDB/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace UpdateDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\projects\images\diskopripajku\2024";
+
+            if (Directory.Exists(directoryPath))
+            {
+                string[] files = Directory.GetFiles(directoryPath);
+
+                foreach (string file in files)
+                {
+                    FileInfo fileInfo = new (file);
+                    Console.WriteLine($"File Name: {fileInfo.Name}");
+                    Console.WriteLine($"File extension: {fileInfo.Extension}");
+                    Console.WriteLine($"Creation Time: {fileInfo.CreationTime}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Directory does not exist.");
+            }
+        }
+    }
+}
